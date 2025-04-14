f6100d1d9cf0c043a244c3a067e2970cebfcfd3e
Margox <margox@foxmail.com>
2025-04-14 22:16:04 +0800

    feat: google analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { Locales } from '@/types/products'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import { notFound } from 'next/navigation'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
@@ -40,6 +41,16 @@ export default async function RootLayout(props: RootLayoutProps) {
           </div>
         </ThemeProvider>
       </body>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KCB1EYWH1H" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-KCB1EYWH1H');
+`}
+      </Script>
     </html>
   )
 }
